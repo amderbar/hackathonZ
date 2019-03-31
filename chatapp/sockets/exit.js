686740fe4f99ab246314baed838d648dedd1ffb7
koheisakata <kohei.sakata@rakus.co.jp>
2019-03-31 17:13:09 +0900

    hackathonZ_answer からsocket関連処理をコピー

--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -2,7 +2,8 @@
 
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-
+    socket.on('exitEvent', function (data) {
+        // 自分以外に送信
+        socket.broadcast.emit('exitEvent', data);
     });
 };
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -2,7 +2,8 @@
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
-    socket.on('', function (data) {
-
+    socket.on('publishEvent', function (data) {
+        // 全員に送信
+        io.sockets.emit('publishEvent', data);
     });
 };
